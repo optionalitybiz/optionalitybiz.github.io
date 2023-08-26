4c81dc09d1dd3ba6e6e810de4ae5566ac7c082ff
jgx02c <97928637+jgx02c@users.noreply.github.com>
2023-08-25 20:53:05 -0700

    update contact

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,5 +1,7 @@
 import classNames from 'classnames';
 import styles from './contact.module.scss';
+import React, {useState }from 'react';
+
 
 export interface ContactProps {
     className?: string;
@@ -10,12 +12,20 @@ export interface ContactProps {
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
 export const Contact = ({ className }: ContactProps) => {
+
+    const [showEmail, setShowEmail] = useState(false);
+
+    const toggleEmail = () => {
+        setShowEmail(!showEmail);
+      };
+
     return (
         <div className={classNames(styles.root, className)}>
             <div className={styles.divContact}>
                 <h3 className={styles.h3Contact}>Partner With Us!</h3>
                 <h1 className={styles.h1Contact}>Let us suit your Businesses needs.</h1>
-                <button className={styles.divButton}>Contact Us</button>
+                <button className={styles.divButton} onClick={toggleEmail}>Contact Us</button>
+                {showEmail && <p className={styles.email}>Email: contact@optionality.biz </p>}
             </div>
         </div>
     );
